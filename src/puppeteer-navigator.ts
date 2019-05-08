45e1f48e1e914adf93164d4a8f4fd07a30d52654
Dakara <chad.meadows@gmail.com>
2019-05-07 22:51:20 -0400

    more docs and features

--- src/puppeteer-navigator.ts
+++ src/puppeteer-navigator.ts
@@ -4,7 +4,7 @@ type SelectorType = number|string|((...args:any)=>boolean)
 type ElementMapFn = (element:ElementAny)=>any
 
 interface NavigatorOptions {
-    waitUntilVisile?: boolean
+    waitUntilVisible?: boolean
     autoWait?: boolean,
     waitAfterAction?: number
     useSimulatedClicks?:boolean
@@ -13,20 +13,35 @@ interface NavigatorOptions {
 export interface ElementAny extends Element {
     [key:string]: any
 }
-export type PageNavigator = ReturnType<typeof makePageNavigator>
-export function makePageNavigator(page:Page, customOptions:NavigatorOptions = {}) {
-    const options = { // default options
+export type PageNavigator = ReturnType<typeof _makePageNavigator>
+export function makePageNavigator(page:Page, customOptions:NavigatorOptions = {}):PageNavigator {return _makePageNavigator(page, customOptions)}
+function _makePageNavigator(currentPage:Page, customOptions:NavigatorOptions = {}) {
+    const options:NavigatorOptions = { // default options
         "waitUntilVisible": true,
         "autoWait": true,
         "waitAfterAction": 0,
         "useSimulatedClicks": true
     }
-    Object.assign(options, customOptions) // override with any custom options
+    updateOptions(customOptions)
+    
+    function updateOptions(customOptions:NavigatorOptions = {}) {
+        Object.assign(options, customOptions) // override with any custom options
+    }
 
+    /**
+     * get the current puppeteer page object
+     */
+    function page() {return currentPage}
+
+    /**
+     * Navigate to URL
+     * @param url 
+     * @param waitCondition 
+     */
     async function gotoUrl(url:string, waitCondition?:SelectorType) {
-        page.goto(url)
+        currentPage.goto(url)
         // wait for the previous navigation to complete
-        const pageResponse = await page.waitForNavigation()
+        const pageResponse = await currentPage.waitForNavigation()
         if (waitCondition)
             await wait(waitCondition)
         else if (options.waitAfterAction) await wait(options.waitAfterAction)
@@ -34,15 +49,30 @@ export function makePageNavigator(page:Page, customOptions:NavigatorOptions = {}
         return pageResponse
     }
     
+    /**
+     * Queries an element using css selector or xpath
+     * Assumes xpath expression starts with '//'
+     * @param selector css selector or xpath
+     */
     async function queryElementHandle(selector: string) {
         if (selector.startsWith('//'))
-            return (await page.$x(selector))[0]
-        return await page.$(selector)
+            return (await currentPage.$x(selector))[0]
+        return await currentPage.$(selector)
     }
 
+    /**
+     * Query element using selector and uses the provided function to map a return value
+     * @param selector css selector
+     * @param valueMapFn function to map element to return value
+     */
     async function queryElement(selector:string, valueMapFn:ElementMapFn) { return (await queryElements(selector, valueMapFn))[0]}
+    /**
+     * Queries elements using selector and uses the provided function to map a list of return values
+     * @param selector css selector
+     * @param valueMapFn function to map elements to values to be returned
+     */
     async function queryElements(selector:string, valueMapFn:ElementMapFn) {
-        const elements = await page.evaluate((selector, valueMapFnText) => {
+        const elements = await currentPage.evaluate((selector, valueMapFnText) => {
             // Functions can not be passed as parameters to the browser page
             // So we pass in the function source text and recreate the function within the browser page
             const valueMapFn = new Function(' return (' + valueMapFnText + ').apply(null, arguments)');
@@ -60,32 +90,49 @@ export function makePageNavigator(page:Page, customOptions:NavigatorOptions = {}
      * @param {*} downloadPath 
      */
     async function setDownloadPath(downloadPath:string) {
-        return await (page as any)._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: downloadPath});
+        return await (currentPage as any)._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: downloadPath});
     }
     
     async function scrollElementToBottom(elementSelector:string, delay:number) {
-        await page.evaluate( selector => {
+        await currentPage.evaluate( selector => {
             const element = document.querySelector(selector);
             element.scrollTop = 100000; // use large number to force to bottom.  TODO - determine if there is an exact way to get this value
         }, elementSelector );
     
-        await page.waitFor(delay);
+        await currentPage.waitFor(delay);
     }
 
+    /**
+     * Waits for element to be visible, function to be true or timeout if number
+     * @param condition css selector, xpath or function
+     */
     async function wait(condition:SelectorType) {
         if (typeof condition === 'string' || typeof condition ==='function') {
-            return await page.waitFor(condition, {visible:options.waitUntilVisible})
+            return await currentPage.waitFor(condition, {visible:options.waitUntilVisible})
         }
         if (typeof condition === 'number') {
-            await page.waitFor(condition)
+            await currentPage.waitFor(condition)
         }
     }
 
-    async function waitFn(selector:string, condition: (element:ElementAny) => boolean, options?: PageFnOptions) {
+    /**
+     * Waits for condition to be true
+     * @param selector css selector, xpath or function
+     * @param condition function that receives element of selector as input.
+     * @param options 'waitAfter' additinoal wait time after condition is true
+     */
+    async function waitFn(selector:string, condition: (element:ElementAny) => boolean, options?: PageFnOptions & {waitAfter:number}) {
         const selectElement = await wait(selector)
-        await page.waitForFunction(condition, options, selectElement as JSHandle)
+        await currentPage.waitForFunction(condition, options, selectElement as JSHandle)
+        if (options && options.waitAfter)
+            await wait(options.waitAfter)
     }
 
+    /**
+     * Performs a click on a HTML field.
+     * @param selector css selector or xpath
+     * @param clickOptions ClickOptions
+     */
     async function click(selector:string, clickOptions?:ClickOptions) {
         if (options.autoWait)
             await wait(selector)
@@ -93,7 +140,7 @@ export function makePageNavigator(page:Page, customOptions:NavigatorOptions = {}
         if (!targetElement) throw new Error('Element not found ' + selector)
 
         if (options.useSimulatedClicks) {
-            await page.evaluate(element => element.click(), targetElement)
+            await currentPage.evaluate(element => element.click(), targetElement)
         } else {
             await targetElement.click(clickOptions)
         }
@@ -101,20 +148,33 @@ export function makePageNavigator(page:Page, customOptions:NavigatorOptions = {}
         if (options.waitAfterAction) await wait(options.waitAfterAction)
     }
 
+    /**
+     * Types text into a HTML field
+     * @param selector css selector
+     * @param text type text into field
+     * @param typeOptions 'delay' sets delay between each key typed
+     */
     async function type(selector:string, text:string, typeOptions?: { delay: number }) {
         if (options.autoWait)
             await wait(selector)
-        await page.type(selector, text, typeOptions)
+        await currentPage.type(selector, text, typeOptions)
 
         if (options.waitAfterAction) await wait(options.waitAfterAction)
     }
 
+    /**
+     * Selects an option within a HTML list.
+     * Filters out any control characters that might be in the list label or value before attempting to match.
+     * 
+     * @param selector css selector
+     * @param selectOption 'value' matches the option value attribute. 'label' matches the option label attribute
+     */
     async function select(selector:string, selectOption: {value?:string, label?:string}) {
         if (options.autoWait)
             await wait(selector)
 
-        const selectElement = await page.$(selector)
-        await page.evaluate((selectElement:Element, selectOption) => {
+        const selectElement = await currentPage.$(selector)
+        await currentPage.evaluate((selectElement:Element, selectOption) => {
             let optionElement:HTMLOptionElement
 
             // find matching option.  Remove any control characters from option values or labels
@@ -132,6 +192,8 @@ export function makePageNavigator(page:Page, customOptions:NavigatorOptions = {}
     }
 
     return {
+        updateOptions,
+        page,
         gotoUrl,
         queryElement,
         queryElements,
--- src/test.ts
+++ src/test.ts
@@ -1,14 +1,20 @@
 import {makePageNavigator, PageNavigator} from "./puppeteer-navigator"
 import puppeteer from "puppeteer"
+import http from 'http'
+import fs from 'fs'
+
+http.createServer(function (request, response) {
+    response.writeHead(200)
+    fs.createReadStream('test/test.html').pipe(response)
+}).listen(8000)
 
 async function run() {
     puppeteer.launch({headless: false, args:['--disable-web-security']}).then(async browser => {
-        const page = await browser.newPage();
+        const page = (await browser.pages())[0]
 
         const navigator = makePageNavigator(page)
-        navigator.gotoUrl('https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select')
-        navigator.wait(3000)
-        navigator.select(`select[name='select']`, {label:'Third Value'})
+        await navigator.gotoUrl('http://localhost:8000')
+        await navigator.select(`#pet-select`, {label:'Spider'})
         //await browser.close();
     });
 }
